336650fa351702e5ec71e9ec5d3a477b5d35e8f8
潘宏钢 <619328391@qq.com>
2018-04-29 14:05:18 +0800

    去掉滤芯特殊字符限制

--- preject/Application/Admin/Model/FiltersModel.class.php
+++ preject/Application/Admin/Model/FiltersModel.class.php
@@ -13,7 +13,6 @@ class FiltersModel extends Model
     // 自动验证
     protected $_validate = array(
         array('filtername','require','滤芯名称不能为空'),
-        array('filtername','/^[a-zA-Z0-9\x{4e00}-\x{9fa5}]{1,660}$/u','请不要输入特殊字符',1,'regex'),
         array('timelife','/^\+?[1-9][0-9]*$/','请输入有效数值',1,'regex'),
         array('flowlife','/^\+?[1-9][0-9]*$/','请输入有效数值',1,'regex'),
     );
