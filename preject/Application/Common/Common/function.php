fd343b5df6619ccaaeff5be7588e296b8788da17
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-04-30 16:00:53 +0800

    梁康伦的修改—押金显示修改

--- preject/Application/Common/Common/function.php
+++ preject/Application/Common/Common/function.php
@@ -11,3 +11,13 @@ function qrcode($url,$size=4){
     Vendor('Phpqrcode.phpqrcode');
     QRcode::png($url,false,QR_ECLEVEL_L,$size,2,false,0xFFFFFF,0x000000);
 }
+
+
+/**
+ * 价格/100   用于页面显示
+ * @param $num
+ * @return string
+ */
+function html_price($num){
+    return number_format(intval(trim($num), 10)/100,2);
+}
